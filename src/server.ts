d103a41c2e9891d4419176e1dd2da648131b14f2
EfrainRodriguez <efrainrg92@gmail.com>
2024-09-22 15:35:55 -0500

    testing mqtt broker in aws

--- src/server.ts
+++ src/server.ts
@@ -1,63 +1,63 @@
-import express from "express";
+// import express from "express";
 
-const app = express();
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is listening on port 3000");
+// });
+
+import * as Aedes from "aedes";
+import net from "net";
+import ws from "websocket-stream";
+import http from "http";
+
+// puero de comunicación del servidor broker mqtt
+const mqttPort = 1883;
+const wsPort = 3000;
+
+// instancia del broker
+const broker = Aedes.createBroker();
+
+// instancia del servidor pasando la instancia del broker
+const mqttServer = net.createServer(broker.handle);
+
+// instancia del servidor http y websocket
+const httpServer = http.createServer();
+const wsServer = ws.createServer(
+  {
+    server: httpServer,
+  },
+  broker.handle as any
+);
+
+// servidor escuchando en el puerto 1883
+mqttServer.listen(mqttPort, () => {
+  console.log("MQTT server listening on port", mqttPort);
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+// servidor http y websocket escuchando en el puerto 8888
+httpServer.listen(wsPort, () => {
+  console.log("Websocket server listening on port", wsPort);
 });
 
-// import * as Aedes from "aedes";
-// import net from "net";
-// import ws from "websocket-stream";
-// import http from "http";
+// evento lanzado cuando se conecta un nuevo cliente
+wsServer.on("connection", () => {
+  console.log("Websocket client connected");
+});
 
-// // puero de comunicación del servidor broker mqtt
-// const mqttPort = 1883;
-// const wsPort = 8080;
+// evento lanzado cuando se conecta un nuevo cliente
+mqttServer.on("connection", (socket) => {
+  console.log("Client connected:", socket.remoteAddress, socket.remotePort);
+});
 
-// // instancia del broker
-// const broker = Aedes.createBroker();
-
-// // instancia del servidor pasando la instancia del broker
-// const mqttServer = net.createServer(broker.handle);
-
-// // instancia del servidor http y websocket
-// const httpServer = http.createServer();
-// const wsServer = ws.createServer(
-//   {
-//     server: httpServer,
-//   },
-//   broker.handle as any
-// );
-
-// // servidor escuchando en el puerto 1883
-// mqttServer.listen(mqttPort, () => {
-//   console.log("MQTT server listening on port", mqttPort);
-// });
-
-// // servidor http y websocket escuchando en el puerto 8888
-// httpServer.listen(wsPort, () => {
-//   console.log("Websocket server listening on port", wsPort);
-// });
-
-// // evento lanzado cuando se conecta un nuevo cliente
-// wsServer.on("connection", () => {
-//   console.log("Websocket client connected");
-// });
-
-// // evento lanzado cuando se conecta un nuevo cliente
-// mqttServer.on("connection", (socket) => {
-//   console.log("Client connected:", socket.remoteAddress, socket.remotePort);
-// });
-
-// // evento que maneja la llegada de un nuevo mensaje desde un cliente
-// broker.on("publish", (packet) => {
-//   console.log("Topic: ", packet.topic, "Payload: ", packet);
-// });
+// evento que maneja la llegada de un nuevo mensaje desde un cliente
+broker.on("publish", (packet) => {
+  console.log("Topic: ", packet.topic, "Payload: ", packet);
+});
 
 // import ws from 'ws';
 
